0ddcde2244758c967d35cba038a66fbb7996e1ca
Sand82 <sandoki@abv.bg>
2023-05-05 17:12:04 +0300

    added style component to expense data component

--- react-exercise/src/components/ExpenseItem.js
+++ react-exercise/src/components/ExpenseItem.js
@@ -1,11 +1,11 @@
 import "./ExpenseItem.css";
 
-import ExspenseDate from "./ExspanseDate";
+import ExpenseDate from "./ExpanseDate";
 
 const ExpenseItem = ({ exspense }) => {
   return (
     <div className="expense-item">
-      <ExspenseDate date={exspense.date} />
+      <ExpenseDate date={exspense.date} />
       <div className="expense-item__description">
         <h2>{exspense.title}</h2>
         <div className="expense-item__price">${exspense.amount}</div>
