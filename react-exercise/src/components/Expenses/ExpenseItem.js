2bb1ce85469520e9f2985269ccda43ac8eb6cbfc
Sand82 <sandoki@abv.bg>
2023-05-09 11:41:26 +0300

    added new form component

--- react-exercise/src/components/Expenses/ExpenseItem.js
+++ react-exercise/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 import ExpenseDate from "./ExpanseDate";
 
 const ExpenseItem = ({ expense }) => {
+  const [title, setTitle] = useState(expense.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={expense.date} />
       <div className="expense-item__description">
-        <h2>{expense.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
