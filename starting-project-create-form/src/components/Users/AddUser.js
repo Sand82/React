c6e49dd9ccb0e97781cfa78237acc209696d2d88
Sand82 <sandoki@abv.bg>
2023-05-15 14:08:22 +0300

    added errors message handler functionality

--- starting-project-create-form/src/components/Users/AddUser.js
+++ starting-project-create-form/src/components/Users/AddUser.js
@@ -8,15 +8,24 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = ({ onAddUsers }) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (e) => {
     e.preventDefault();
 
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Place enter a valid name and age (non-empty values)",
+      });
       return;
     }
 
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Place enter a valid age (greater than 0)",
+      });
       return;
     }
 
@@ -39,9 +48,19 @@ const AddUser = ({ onAddUsers }) => {
     setEnteredAge(e.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <>
-      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
