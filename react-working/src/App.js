1f171113421f66f25c81ab79fc3d0cf943cd48a8
Sand82 <sandoki@abv.bg>
2023-05-17 17:58:01 +0300

    added button in app component

--- react-working/src/App.js
+++ react-working/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import Button from "./components/UI/Button/Button";
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const clickHandler = () => {
+    setShowParagraph((state) => !state);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new !</p>}
+      <Button onClick={clickHandler}> Toggle </Button>
     </div>
   );
 }
