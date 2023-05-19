8ce8ac003e4f3cdccec0c80764ec18d330ced5b9
Sand82 <sandoki@abv.bg>
2023-05-19 17:21:15 +0300

    added async await on get request

--- http-exercise/src/App.js
+++ http-exercise/src/App.js
@@ -4,23 +4,37 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  const [movie, setMovie] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = () => {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => response.json())
-      .then((data) => {
-        const dataTransformedMovies = data.results.map((x) => {
-          return {
-            id: x.episode_id,
-            title: x.title,
-            openingText: x.opening_crawl,
-            releaseDate: x.release_date,
-          };
-        });
-        setMovie(dataTransformedMovies);
+  async function fetchMoviesHandler() {
+    setError(null);
+    setIsLoading(true);
+    try {
+      let response = await fetch("https://swapi.dev/api/films");
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      let data = await response.json();
+
+      const dataTransformedMovies = data.results.map((x) => {
+        return {
+          id: x.episode_id,
+          title: x.title,
+          openingText: x.opening_crawl,
+          releaseDate: x.release_date,
+        };
       });
-  };
+      setMovies(dataTransformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+
+    setIsLoading(false);
+  }
 
   return (
     <React.Fragment>
@@ -28,7 +42,10 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movie} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>Found no movies</p>}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && <h2>Loading...</h2>}
       </section>
     </React.Fragment>
   );
