44214beec908cd91a7ac64b1c8c5b320592820d1
Sand82 <sandoki@abv.bg>
2023-05-23 15:25:27 +0300

    added fech data functionality

--- practise-http-and-forms/src/components/Meals/AvailableMeals.js
+++ practise-http-and-forms/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,36 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
+import { useState, useEffect } from "react";
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  const getMeals = async () => {
+    let response = await fetch(
+      "https://react-http-b8057-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+    );
+
+    const responseData = await response.json();
+    const currentMeals = [];
+
+    for (const key in responseData) {
+      currentMeals.push({
+        id: key,
+        name: responseData[key].name,
+        description: responseData[key].description,
+        price: Number(responseData[key].price),
+      });
+    }
+    setMeals(currentMeals);
+  };
+
+  useEffect(() => {
+    getMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
