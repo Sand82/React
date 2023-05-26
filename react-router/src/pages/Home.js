c0802e76623cdf813c8f5897a593d2c83a723e68
Sand82 <sandoki@abv.bg>
2023-05-26 16:43:34 +0300

    added ue navigate hook

--- react-router/src/pages/Home.js
+++ react-router/src/pages/Home.js
@@ -1,11 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate("/products");
+  };
+
   return (
     <>
       <div>My Home Page</div>
       <p>
         Go to <Link to="/products">the list of products</Link>.
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Products</button>
       </p>
     </>
   );
