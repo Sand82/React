bcff754948c6d17646480a220f78573d9677b3ac
Sand82 <sandoki@abv.bg>
2023-05-30 15:36:06 +0300

    added remove functionality

--- advanced-routing-exercise-project/frontend/src/components/EventForm.js
+++ advanced-routing-exercise-project/frontend/src/components/EventForm.js
@@ -1,22 +1,38 @@
-import { Form, useNavigate } from "react-router-dom";
+import {
+  Form,
+  useNavigate,
+  useNavigation,
+  useActionData,
+} from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+  const data = useActionData();
   const navigate = useNavigate();
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
+
   function cancelHandler() {
     navigate("..");
   }
 
   return (
     <Form method="post" className={classes.form}>
+      {data && data.errors && (
+        <ul>
+          {Object.values(data.errors).map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">Title</label>
         <input
           id="title"
           type="text"
           name="title"
-          required
           defaultValue={event ? event.title : ""}
         />
       </p>
@@ -26,7 +42,6 @@ function EventForm({ method, event }) {
           id="image"
           type="url"
           name="image"
-          required
           defaultValue={event ? event.image : ""}
         />
       </p>
@@ -36,7 +51,6 @@ function EventForm({ method, event }) {
           id="date"
           type="date"
           name="date"
-          required
           defaultValue={event ? event.date : ""}
         />
       </p>
@@ -46,15 +60,16 @@ function EventForm({ method, event }) {
           id="description"
           name="description"
           rows="5"
-          required
           defaultValue={event ? event.description : ""}
         />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Save"}
+        </button>
       </div>
     </Form>
   );
