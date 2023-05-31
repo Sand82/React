691e88e05e66e3e6d20dd37b2693bdf6e73132f6
Sand82 <sandoki@abv.bg>
2023-05-31 12:37:44 +0300

    added token expiration

--- authentication/frontend/src/util/auth.js
+++ authentication/frontend/src/util/auth.js
@@ -1,8 +1,38 @@
+import { redirect } from "react-router-dom";
+
+export function getTokenDuration() {
+  const storedExpirationData = localStorage.getItem("expiration");
+  const expirationDate = new Date(storedExpirationData);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getItem();
+
+  return duration;
+}
+
 export function getAuthToken() {
   const token = localStorage.getItem("token");
+
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
+
   return token;
 }
 
 export function tokenLoader() {
   return getAuthToken();
+}
+
+export function chackAuthloader() {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect("/auth");
+  }
 }
