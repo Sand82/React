3e3a6b2b641341babffc96932199440aa4602b67
Sand82 <sandoki@abv.bg>
2025-01-28 21:14:00 +0200

    added useLocalStorage custom hook

---
+++ samurider/samurider/src/hooks/useLocalStorage.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export const useLocalStorage = (key) => {
+  const [value, setValue] = useState({});
+
+  useEffect(() => {
+    let currUserInfo = localStorage.getItem(key);
+    let currValue = currUserInfo ? JSON.parse(currUserInfo) : {};
+    setValue(currValue);
+  }, []);
+
+  const setLocalStorageValue = (newValue) => {
+    localStorage.setValue(key, JSON.stringify(newValue));
+    setValue(newValue);
+  };
+
+  return [value, setLocalStorageValue];
+};
