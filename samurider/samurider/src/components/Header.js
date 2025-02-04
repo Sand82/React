ba7501d369f80933b98fada4eaceb9f5aa988361
Sand82 <sandoki@abv.bg>
2025-02-04 20:10:09 +0200

    added logout functionality

--- samurider/samurider/src/components/Header.js
+++ samurider/samurider/src/components/Header.js
@@ -1,27 +1,45 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+
+import { AuthContext } from "../contexts/AuthContext.js";
+import * as AuthService from "../services/AuthService.js";
 
 const Header = () => {
+  const { user, userLogout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const logoutHeandler = () => {
+    AuthService.logout(user.accessToken);
+    userLogout();
+
+    navigate("/");
+  };
+
   return (
     <header>
-      {/* Navigation */}
       <Link id="logo" to="/">
         <img id="logo-img" src="./images/logo.png" alt="" />
       </Link>
       <nav>
+        {user.email && <div className="user-email">{user.email}</div>}
         <div>
           <Link to="/catalog">Motorcycles</Link>
           <Link to="#">Search</Link>
         </div>
-        {/* Logged-in users */}
-        <div className="user">
-          <Link to="#">Add Motorcycle</Link>
-          <Link to="#">Logout</Link>
-        </div>
-        {/* Guest users */}
-        <div className="guest">
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </div>
+
+        {user.email ? (
+          <div className="user">
+            <Link to="#">Add Motorcycle</Link>
+            <Link onClick={logoutHeandler} to="/">
+              Logout
+            </Link>
+          </div>
+        ) : (
+          <div className="guest">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+        )}
       </nav>
     </header>
   );
--- samurider/samurider/src/components/Register.js
+++ samurider/samurider/src/components/Register.js
@@ -1,30 +1,150 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import * as AuthValidator from "../validators/AuthValidator.js";
+import * as StateValidator from "../validators/StateValidator.js";
+import * as AuthService from "../services/AuthService.js";
+
 const Register = () => {
+  const [registerCredential, setRegisterCredential] = useState({
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  const [error, setError] = useState({
+    email: false,
+    password: false,
+    repeatPassword: false,
+  });
+
+  const [requestAndOtherError, setRequestAndOtherError] = useState({
+    message: "",
+    hasError: false,
+  });
+
+  const navigate = useNavigate();
+
+  const registerSubmitHeandler = (e) => {
+    e.preventDefault();
+
+    if (StateValidator.validateState(error, registerCredential)) {
+      setRequestAndOtherError((error) => ({
+        message: "Required valid email and password!",
+        hasError: true,
+      }));
+
+      return;
+    }
+
+    let isNotValidConfirmation = AuthValidator.passwordConfirmation(
+      registerCredential.password,
+      registerCredential.repeatPassword
+    );
+
+    if (isNotValidConfirmation) {
+      setRequestAndOtherError((error) => ({
+        message: "Password and password confirmation should be equal!",
+        hasError: true,
+      }));
+
+      return;
+    }
+
+    let registerObject = {
+      email: registerCredential.email,
+      password: registerCredential.password,
+    };
+
+    AuthService.register(registerObject).then((response) => {
+      if (response.code) {
+        setRequestAndOtherError((error) => ({
+          message: response.message,
+          hasError: true,
+        }));
+        return;
+      }
+
+      navigate("/login");
+    });
+  };
+
+  const registerChangeHeandler = (e) => {
+    setRegisterCredential((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const registerError = (e) => {
+    let currField = e.target.name;
+
+    let isFieldNotValid = AuthValidator.credentialErrors(
+      currField,
+      e.target.value
+    );
+
+    setError((error) => ({
+      ...error,
+      [currField]: isFieldNotValid,
+    }));
+  };
+
   return (
     <section id="register">
       <div className="form">
         <h2>Register</h2>
-        <form className="register-form">
+        {requestAndOtherError.hasError && (
+          <span className="server-error-massage">
+            {requestAndOtherError.message}
+          </span>
+        )}
+        <form className="register-form" onSubmit={registerSubmitHeandler}>
           <input
             type="text"
             name="email"
             id="register-email"
             placeholder="email"
+            value={registerCredential.email}
+            onChange={registerChangeHeandler}
+            onBlur={registerError}
           />
+          {error.email && (
+            <span className="field-error-message">
+              Email address is not valid!
+            </span>
+          )}
           <input
             type="password"
             name="password"
             id="register-password"
             placeholder="password"
+            value={registerCredential.password}
+            onChange={registerChangeHeandler}
+            onBlur={registerError}
           />
+          {error.password && (
+            <span className="field-error-message">
+              Password should be more than 6 cheracters!
+            </span>
+          )}
           <input
             type="password"
-            name="re-password"
+            name="repeatPassword"
             id="repeat-password"
             placeholder="repeat password"
+            value={registerCredential.repeatPassword}
+            onChange={registerChangeHeandler}
+            onBlur={registerError}
           />
+          {error.repeatPassword && (
+            <span className="field-error-message">
+              Password should be more than 6 cheracters!
+            </span>
+          )}
           <button type="submit">register</button>
           <p className="message">
-            Already registered? <a href="#">Login</a>
+            Already registered? <Link to="/login">Login</Link>
           </p>
         </form>
       </div>
